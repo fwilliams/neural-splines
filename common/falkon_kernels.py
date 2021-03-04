a3ec9d95bb114bdc621424acdf755b8d834ac365
Francis Williams <francis@fwilliams.info>
2021-03-04 17:09:47 -0500

    cupy kernel preconditioner

--- common/falkon_kernels.py
+++ common/falkon_kernels.py
@@ -184,7 +184,7 @@ class ArcCosineKernel(Kernel, KeopsKernelMixin, ABC, DirectKernelMixin):
 
 
 class NeuralTangentKernel(Kernel, KeopsKernelMixin, ABC, DirectKernelMixin):
-    kernel_type = "dot-product"
+    kernel_type = "angle"
 
     def __init__(self, variance: float = 1.0, opt: Optional[FalkonOptions] = None):
         super().__init__("NeuralTangentKernel", self.kernel_type, opt)
@@ -231,37 +231,83 @@ class NeuralTangentKernel(Kernel, KeopsKernelMixin, ABC, DirectKernelMixin):
     def _prepare(self, X1, X2, **kwargs):
         if self.debug:
             print(f"NeuralTangentKernel._prepare(X1={X1.shape}, X2={X2.shape}, *kwargs)")
-        return [torch.norm(X1, dim=-1).unsqueeze(1), torch.norm(X2, dim=-1).unsqueeze(0)]
+        return []
 
     def _prepare_sparse(self, X1: SparseTensor, X2: SparseTensor):
         raise NotImplementedError("NeuralTangentKernel does not implement sparse prepare")
 
     def _apply(self, X1: torch.Tensor, X2: torch.Tensor, out: torch.Tensor):
-        X2 = X2.T  # This is passed in transposed... ugh
-        arg1 = torch.zeros(X1.shape[0], X2.shape[0], X1.shape[-1]).to(out)
-        arg2 = torch.zeros(X1.shape[0], X2.shape[0], X1.shape[-1]).to(out)
-        print(out.shape, X1.shape, X2.shape)
-        nx1, nx2 = torch.norm(X1, dim=-1, keepdim=True).unsqueeze(1), torch.norm(X2, dim=-1, keepdim=True).unsqueeze(0)
+        if self.debug:
+            print(f"NeuralTangentKernel._apply(X1={X1.shape}, X2={X2.shape}, out={out.shape})")
+        import cupy as cp
+        from torch.utils.dlpack import to_dlpack
+        from torch.utils.dlpack import from_dlpack
 
-        print(nx1.shape, nx2.shape, X1.shape, X2.shape)
-        # |X| * Y
-        arg2.add_(nx1)
-        arg2.mul_(X2.unsqueeze(0))
+        X2 = X2.T.contiguous()  # This is passed in transposed... ugh
 
-        # |Y| * X
-        arg1.add_(nx2)  # [n, m, d]
-        arg1.mul_(X1.unsqueeze(1))  # [n, m, d]
-        arg1.sub_(arg2)  # |Y| * X - |X| * Y
-        out.add_(arg1.norm(dim=-1))
+        # Convert X1 and X2 to CuPy arrays.
+        x1cp = cp.fromDlpack(to_dlpack(X1))
+        x2cp = cp.fromDlpack(to_dlpack(X2))
+        outcp = cp.fromDlpack(to_dlpack(out))
 
-        # |Y| * X
-        arg1.zero_()
-        arg1.add_(nx2)  # [n, m, d]
-        arg1.mul_(X1.unsqueeze(1))  # [n, m, d]
-        arg1.add_(arg2)  # |Y| * X + |X| * Y
-        out.atan2_(arg1.norm(dim=-1))
+        kernel_code = r'''
+        #define PI (DTYPE) (3.1415926535897932384626433832795028841971693993751058209749445923078164062)
+        #define ONE (DTYPE) (1.0)
+        extern "C" __global__
+        void stable_kernel(const DTYPE* x1, const DTYPE* x2, DTYPE* out, DTYPE variance, const int N, int M, int D) {
+            const int I = (blockIdx.x * blockDim.x) + threadIdx.x;
+            const int J = (blockIdx.y * blockDim.y) + threadIdx.y;
+    
+            if (I >= N || J >= M) {
+                return;
+            }
+    
+            DTYPE norm_x = (DTYPE) 0.0; //normf(D, &x1[I*D]);
+            DTYPE norm_y = (DTYPE) 0.0; //normf(D, &x2[J*D]);
+    
+            #pragma unroll 
+            for (int k = 0; k < D; k += 1) {
+                norm_x = fma(x1[I * D + k], x1[I * D + k], norm_x);
+                norm_y = fma(x2[J * D + k], x2[J * D + k], norm_y);
+            }
+            norm_x = sqrt(norm_x);
+            norm_y = sqrt(norm_y);
+    
+            DTYPE arg1 = (DTYPE) 0.0;
+            DTYPE arg2 = (DTYPE) 0.0;
+    
+            #pragma unroll
+            for (int k = 0; k < D; k += 1) {
+                DTYPE x1_ik = x1[I * D + k];
+                DTYPE x2_jk = x2[J * D + k];
+                DTYPE a1 = norm_y * x1_ik - norm_x * x2_jk;
+                DTYPE a2 = norm_y * x1_ik + norm_x * x2_jk;
+    
+                arg1 = fma(a1, a1, arg1);
+                arg2 = fma(a2, a2, arg2);
+            }
+            arg1 = sqrt(arg1);
+            arg2 = sqrt(arg2);
+    
+            DTYPE angle = 2.0 * atan2(arg1, arg2);
+            
+            DTYPE norm_xy = norm_x * norm_y;
+            DTYPE cos_angle = cos(angle);
+            DTYPE sin_angle = sin(angle);
+            DTYPE K = norm_xy * (sin_angle + (ONE + variance) * (PI - angle) * cos_angle) / PI;
+            out[I * M + J] = K;
+        }
+        '''
+        str_dtype = "float" if X1.dtype == torch.float32 else "double"
+        kernel_code = kernel_code.replace("DTYPE", str_dtype)
+        kernel = cp.RawKernel(kernel_code, 'stable_kernel')
 
-        out.mul_(2.0)
+        pt_dim = X1.shape[1]
+        dims = X1.shape[0], X2.shape[0]
+        threads = (16, 16)  # TODO: Maybe hardcoding this is bad
+        blocks = tuple((dims[i] + threads[i] - 1) // threads[i] for i in range(2))
+        kernel(threads, blocks, (x1cp, x2cp, outcp, self.variance, dims[0], dims[1], pt_dim))
+        out = from_dlpack(outcp.toDlpack())
 
     def _apply_sparse(self, X1: SparseTensor, X2: SparseTensor, out: torch.Tensor):
         raise NotImplementedError("NeuralTangentKernel does not implement sparse apply")
@@ -269,23 +315,6 @@ class NeuralTangentKernel(Kernel, KeopsKernelMixin, ABC, DirectKernelMixin):
     def _finalize(self, A: torch.Tensor, d):
         if self.debug:
             print(f"NeuralTangentKernel._finalize(A={A.shape}, d)")
-        C = torch.cos(A)
-        S = torch.sin(A)
-
-        # (1.0 + var) * (pi - theta)
-        A.mul_(-1.0)
-        A.add_(np.pi)
-        A.mul_(1.0 + self.variance)
-
-        # sin(theta) + (1.0 + var) * (pi - theta) * cos(theta)
-        A.mul_(C)
-        A.add_(S)
-
-        del C, S
-        nx1, nx2 = [_.to(A) for _ in d]
-        A.mul_(nx1)  # |X| * (J_1 + J_2)
-        A.mul_(nx2)  # |X| * |Y| * (J_1 + J_2)
-        A.div_(np.pi)  # |X| * |Y| * J_1 / pi
         return A
 
     def direct_kernel(self, X1, X2):
