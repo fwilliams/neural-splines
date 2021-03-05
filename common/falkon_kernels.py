6c6692444dd5cbc6505ee09da710fd89401496cf
Francis Williams <francis@fwilliams.info>
2021-03-05 16:56:31 -0500

    bring back the debug prints

--- common/falkon_kernels.py
+++ common/falkon_kernels.py
@@ -338,14 +338,14 @@ class NeuralTangentKernel(Kernel, KeopsKernelMixin, ABC, DirectKernelMixin):
             out[:, :] = from_dlpack(outcp.toDlpack())
             del outcp
         # out = from_dlpack(outcp.toDlpack())
-        # print(out)
-        # rand_idx_i, rand_idx_j = np.random.randint(X1.shape[0]), np.random.randint(X2.shape[0])
-        # xi, xj = X1[rand_idx_i].detach().cpu().numpy(), X2[rand_idx_j].detach().cpu().numpy()
-        # nxi, nxj = np.linalg.norm(xi), np.linalg.norm(xj)
-        # angle1, angle2 = np.linalg.norm(nxj * xi - nxi * xj), np.linalg.norm(nxj * xi + nxi * xj)
-        # angle = 2.0 * np.arctan2(angle1, angle2)
-        # kij = nxi * nxj * (np.sin(angle) + (1.0 + self.variance) * (np.pi - angle) * np.cos(angle)) / np.pi
-        # print(np.abs(kij - out[rand_idx_i, rand_idx_j].item()))
+        print(out)
+        rand_idx_i, rand_idx_j = np.random.randint(X1.shape[0]), np.random.randint(X2.shape[0])
+        xi, xj = X1[rand_idx_i].detach().cpu().numpy(), X2[rand_idx_j].detach().cpu().numpy()
+        nxi, nxj = np.linalg.norm(xi), np.linalg.norm(xj)
+        angle1, angle2 = np.linalg.norm(nxj * xi - nxi * xj), np.linalg.norm(nxj * xi + nxi * xj)
+        angle = 2.0 * np.arctan2(angle1, angle2)
+        kij = nxi * nxj * (np.sin(angle) + (1.0 + self.variance) * (np.pi - angle) * np.cos(angle)) / np.pi
+        print(np.abs(kij - out[rand_idx_i, rand_idx_j].item()))
 
     def _apply_sparse(self, X1: SparseTensor, X2: SparseTensor, out: torch.Tensor):
         raise NotImplementedError("NeuralTangentKernel does not implement sparse apply")
