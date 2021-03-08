ec0bfa06775f4788b1ca6eb21a5e5c4b8ea4481b
Francis Williams <francis@fwilliams.info>
2021-03-08 12:54:59 -0500

    fix prints

--- common/falkon_kernels.py
+++ common/falkon_kernels.py
@@ -315,8 +315,7 @@ class NeuralTangentKernel(Kernel, KeopsKernelMixin, ABC, DirectKernelMixin):
         x1cp = cp.fromDlpack(to_dlpack(X1))
         x2cp = cp.fromDlpack(to_dlpack(X2))
         print("OUT STRIDE", out.stride())
-        print("OUT MEM FMT CONTIG?", out.is_contiguous(torch.contiguous_format))
-        print("OUT MEM FMT CHAN LST?", out.is_contiguous(torch.channels_last))
+        print("OUT IS CONTIG?", out.is_contiguous())
         if out.is_contiguous():
             print("HERE")
             outcp = cp.fromDlpack(to_dlpack(out))
