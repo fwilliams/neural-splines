aac442e0e9d3319fd990adf0624c1c9d61b12c41
Francis Williams <francis@fwilliams.info>
2021-03-08 15:30:22 -0500

    More prints

--- common/falkon_kernels.py
+++ common/falkon_kernels.py
@@ -318,6 +318,7 @@ class NeuralTangentKernel(Kernel, KeopsKernelMixin, ABC, DirectKernelMixin):
         print("OUT STRIDE", out.stride())
         print("OUT IS CONTIG?", out.is_contiguous())
         outcp = cp.fromDlpack(to_dlpack(out))
+        print("OUT CUPY SHAPE", outcp.shape)
         print("OUT CUPY STRIDE", outcp.strides)
         print("OUT CUPY FLAGS?", outcp.flags)
         # if out.is_contiguous():
