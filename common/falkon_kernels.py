70d219dc8687ebad866ac0ad39df8ee7a8dc0ba7
Francis Williams <francis@fwilliams.info>
2021-03-08 16:25:36 -0500

    More prints

--- common/falkon_kernels.py
+++ common/falkon_kernels.py
@@ -345,7 +345,6 @@ class NeuralTangentKernel(Kernel, KeopsKernelMixin, ABC, DirectKernelMixin):
             print("COPYING CUPY OUT TO PYTORCH")
             out.copy_(from_dlpack(outcp.toDlpack()))
             del outcp
-        out = from_dlpack(outcp.toDlpack())
         print("OUT PYTORCH\n", out)
         rand_idx_i, rand_idx_j = np.random.randint(X1.shape[0]), np.random.randint(X2.shape[0])
         xi, xj = X1[rand_idx_i].detach().cpu().numpy(), X2[rand_idx_j].detach().cpu().numpy()
