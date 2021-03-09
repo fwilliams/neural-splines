a4137e22fc1f92821691e10631dabcd9e5cc766b
Francis Williams <francis@fwilliams.info>
2021-03-09 11:58:11 -0500

    Try this?

--- common/falkon_kernels.py
+++ common/falkon_kernels.py
@@ -358,7 +358,7 @@ class NeuralTangentKernel(Kernel, KeopsKernelMixin, ABC, DirectKernelMixin):
         out_dlpack = torch.utils.dlpack.from_dlpack(outcp_dlpack)
         out.copy_(out_dlpack)
         # out[:, :] = out_dlpack
-        # print("OUT PYTORCH\n", out)
+        print("OUT PYTORCH\n", out)
 
         # rand_idx_i, rand_idx_j = np.random.randint(X1.shape[0]), np.random.randint(X2.shape[0])
         # xi, xj = X1[rand_idx_i].detach().cpu().numpy(), X2[rand_idx_j].detach().cpu().numpy()
