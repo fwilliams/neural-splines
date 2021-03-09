1c4d1cdba0be0da8900c17345461200f41b10e11
Francis Williams <francis@fwilliams.info>
2021-03-09 12:06:11 -0500

    rm prints

--- common/falkon_kernels.py
+++ common/falkon_kernels.py
@@ -332,34 +332,31 @@ class NeuralTangentKernel(Kernel, KeopsKernelMixin, ABC, DirectKernelMixin):
         kernel_code = kernel_code.replace("DTYPE", str_dtype)
         kernel = cp.RawKernel(kernel_code, 'stable_kernel')
 
-        print("X1 CONTIG", X1.is_contiguous())
-        print("X2 CONTIG", X2.is_contiguous())
-        print("X2.T CONTIG", X2.T.is_contiguous())
-        print("OUT CONTIG", out.is_contiguous())
+        # The .contiguous should be a no-op in both these cases, but add them in for good measure
         X1 = X1.contiguous()
-        X2 = X2.T.contiguous()  # This is passed in transposed
+        X2 = X2.T.contiguous()
 
         # Convert X1 and X2 to CuPy arrays.
-        # print("ALLOCATING CUPY ARRAY")
         x1cp = cp.fromDlpack(torch.utils.dlpack.to_dlpack(X1))
         x2cp = cp.fromDlpack(torch.utils.dlpack.to_dlpack(X2))
         with cp.cuda.Device(out.device.index):
             outcp = cp.zeros((out.shape[0], out.shape[1]), dtype=cupy_dtype)
 
+        # Run the CUDA kernel to build the matrix K
         pt_dim = int(X1.shape[1])
         dims = int(X1.shape[0]), int(X2.shape[0])
         threads_per_block = (16, 16)  # TODO: Maybe hardcoding this is bad
         blocks_per_grid = tuple((dims[i] + threads_per_block[i] - 1) // threads_per_block[i] for i in range(2))
         kernel(blocks_per_grid, threads_per_block, (x1cp, x2cp, outcp, self.variance, dims[0], dims[1], pt_dim))
-        cp.cuda.stream.get_current_stream().synchronize()
+        cp.cuda.stream.get_current_stream().synchronize()  # Need to synchronize so we can copy to PyTorch
 
         # print("COPYING CUPY OUT TO PYTORCH")
         # print("OUT CUPY\n", outcp)
+        # Copy the kernel back into the output PyTorch tensor
         outcp_dlpack = outcp.toDlpack()
         out_dlpack = torch.utils.dlpack.from_dlpack(outcp_dlpack)
         out.copy_(out_dlpack)
-        # out[:, :] = out_dlpack
-        print("OUT PYTORCH\n", out)
+        # print("OUT PYTORCH\n", out)
 
         # rand_idx_i, rand_idx_j = np.random.randint(X1.shape[0]), np.random.randint(X2.shape[0])
         # xi, xj = X1[rand_idx_i].detach().cpu().numpy(), X2[rand_idx_j].detach().cpu().numpy()
