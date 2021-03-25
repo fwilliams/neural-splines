245ff3e5b7274b4db8d2d762a40532ee89be5669
Francis Williams <francis@fwilliams.info>
2021-03-25 16:02:19 -0400

    debug

--- fit-grid.py
+++ fit-grid.py
@@ -41,11 +41,12 @@ def reconstruct_on_grid(model, full_grid_width, full_bbox, cell_bbox, cell_bbox_
     # print("FULL BBOX", full_bbmin, full_bbsize)
     # print("CELL BBOX", cell_bbmin, cell_bbsize)
 
-    print(full_bbsize, full_grid_width)
+    print("  full_bbsize, full_grid_width", full_bbsize, full_grid_width)
     full_grid_size = np.round(full_bbsize * full_grid_width).astype(np.int64)
+    print("  full_grid_size", full_grid_size)
 
-    cell_bbmin_rel = (cell_bbmin - full_bbmin) / cell_bbsize
-    cell_bbmax_rel = (cell_bbmin + cell_bbsize - full_bbmin) / cell_bbsize
+    cell_bbmin_rel = (cell_bbmin - full_bbmin) / full_bbsize
+    cell_bbmax_rel = (cell_bbmin + cell_bbsize - full_bbmin) / full_bbsize
 
     # print("RELATIVE BBOX", cell_bbmin_rel, cell_bbmax_rel)
 
