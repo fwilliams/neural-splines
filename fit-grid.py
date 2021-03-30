4fe432c1888a5634e49d21515ff2de7b080125c7
Francis Williams <francis@fwilliams.info>
2021-03-30 12:06:08 -0400

    Quantize bounding box to grid

--- fit-grid.py
+++ fit-grid.py
@@ -274,8 +274,8 @@ def main():
                 # Bounding box of the cell in world coordinates
                 cell_bbox = scaled_bbox[0] + cell_vox_origin * voxel_size, cell_vox_size * voxel_size
 
-                cell_bboxes[c_i][c_j].append(cell_bbox)
-                cell_vox_grids[c_i][c_j].append((cell_vox_origin, cell_vox_size))
+                cell_bboxes[c_i][c_j].append(cell_bbox.clone())
+                cell_vox_grids[c_i][c_j].append((cell_vox_origin.clone(), cell_vox_size.clone()))
 
                 # If there are no points in this region, then skip it
                 mask_cell = points_in_bbox(x, cell_bbox)
